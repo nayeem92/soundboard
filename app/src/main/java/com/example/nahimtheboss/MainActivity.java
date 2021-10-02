d1310910ca53e3d81e79780b1420b79237626aa8
Abdun Nayeem Khan <nayeem0073297@gmail.com>
2021-10-02 19:22:40 +0600

    first

---
+++ app/src/main/java/com/example/nahimtheboss/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.nahimtheboss;
+
+
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private SoundPool soundPool;
+    private int sound1, sound2, sound3, sound4, sound5;
+    private int sound3StreamId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(5)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        sound1 = soundPool.load(this, R.raw.sound1, 1);
+        sound2 = soundPool.load(this, R.raw.sound2, 1);
+        sound3 = soundPool.load(this, R.raw.sound3, 1);
+        sound4 = soundPool.load(this, R.raw.sound4, 1);
+        sound5 = soundPool.load(this, R.raw.sound5, 1);
+
+    }
+
+    public void playSound(View v) {
+        switch (v.getId()) {
+            case R.id.button_sound1:
+                soundPool.play(sound1, 1, 1, 0, 0, 1);
+                //soundPool.pause(sound3StreamId);
+                soundPool.autoPause();
+                break;
+            case R.id.button_sound2:
+                soundPool.play(sound2, 1, 1, 0, 0, 1);
+                break;
+            case R.id.button_sound3:
+                sound3StreamId = soundPool.play(sound3, 1, 1, 0, 0, 1);
+                break;
+            case R.id.button_sound4:
+                soundPool.play(sound4, 1, 1, 0, 0, 1);
+                break;
+            case R.id.button_sound5:
+                soundPool.play(sound5, 1, 1, 0, 0, 1);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
+    }
+}
